d89e142e88a6a705cf9aad03063ebf22ee2aed3b
codingclemo <clemens.kozmich@gmail.com>
2018-04-16 18:42:40 +0200

    added ahmad magic

--- src/codemetrics/JARInspect.java
+++ src/codemetrics/JARInspect.java
@@ -10,33 +10,60 @@ import java.util.jar.JarFile;
 import java.util.jar.JarEntry;
 
 public class JARInspect {
+	/*
+		1- Break it first, break all the time
+		2- Read the files first
+		3- Print it to know whats inside
+		4- Reucrsive if its .jar
+		5- Read the content (Open stream)
+		6- Analzye -> think more about it do not it immediately
+
+
+
+
+
+	 */
 
 	public static void main(String[] args) throws IOException {
 			
-		String fileName = "heap.jar";
-		importJar(fileName);
+		//String fileName = "heap.jar";
+		//importJar(fileName);
+
+		ArrayList<String> files;
+		for(int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+			files.add(args[i]);
+		}
+
+		for (int i = 0; i < files.size(); i++) {
+			//importJar(files.get(i));
+		}
 	}
 	
 	public static void importJar(String fileName) throws IOException {
 		
-		JarFile jf = new JarFile(new File(fileName));
-		
+		//JarFile jf = new JarFile(new File(fileName));
+
+		File file = new File(fileName.toString());
+		JarFile jf = new JarFile(file);
+		System.out.println(jf.toString());
+
 //		int NrOfMethodsTotal = 0;
-		ArrayList<String> classNames = new ArrayList<>();
+		//ArrayList<String> classNames = new ArrayList<>();
 		
-		String fileDirectory = System.getProperty("user.dir");
-		URL JARLocation = new URL("file://" + fileDirectory + "/" + fileName);
-		System.out.println(JARLocation);
-		URLClassLoader urlCL = new URLClassLoader(
-									new URL[] { JARLocation }
-									);
+		//String fileDirectory = System.getProperty("user.dir");
+		//URL JARLocation = new URL("file://" + fileDirectory + "/" + fileName);
+		//System.out.println(JARLocation);
+		//URLClassLoader urlCL = new URLClassLoader(
+		//							new URL[] { JARLocation }
+		//							);
 		
 		// go through all classes and gather data for metrics 
 		// iterate over all classes
 				//getClasses(jf, classNames);
 		// calculate cumulative metrics for jar
 		
-		urlCL.close();
+		//urlCL.close();
 	}
 	
 	private static void getClasses(JarFile jf, Arraylist<String> classNames) throws IOException {
