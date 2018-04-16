5feb1cffbac5d1423e13bfdaa1dab64f57607248
codingclemo <clemens.kozmich@gmail.com>
2018-04-16 22:57:18 +0200

    Added URLClassLoader to create a URL and be able to load a class with it later.

--- src/codemetrics/JARInspect.java
+++ src/codemetrics/JARInspect.java
@@ -2,9 +2,11 @@ package codemetrics;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.jar.JarFile;
 import java.util.jar.JarEntry;
@@ -19,67 +21,89 @@ public class JARInspect {
 		6- Analzye -> think more about it do not it immediately
 
 
-
-
-
 	 */
 
 	public static void main(String[] args) throws IOException {
-			
-		//String fileName = "heap.jar";
-		//importJar(fileName);
 
-		ArrayList<String> files;
+		// get filenames from arguments list
+		ArrayList<String> files = new ArrayList<>();
 		for(int i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
 			files.add(args[i]);
 		}
-
+		
+		// go through filenames and analyse them
 		for (int i = 0; i < files.size(); i++) {
-			//importJar(files.get(i));
+			System.out.println(i);
+			checkJar(files.get(i));
 		}
 	}
 	
-	public static void importJar(String fileName) throws IOException {
+	public static void checkJar(String fileName) throws IOException {
 		
-		//JarFile jf = new JarFile(new File(fileName));
-
 		File file = new File(fileName.toString());
-		JarFile jf = new JarFile(file);
-		System.out.println(jf.toString());
-
-//		int NrOfMethodsTotal = 0;
-		//ArrayList<String> classNames = new ArrayList<>();
-		
-		//String fileDirectory = System.getProperty("user.dir");
-		//URL JARLocation = new URL("file://" + fileDirectory + "/" + fileName);
-		//System.out.println(JARLocation);
-		//URLClassLoader urlCL = new URLClassLoader(
-		//							new URL[] { JARLocation }
-		//							);
-		
-		// go through all classes and gather data for metrics 
-		// iterate over all classes
-				//getClasses(jf, classNames);
-		// calculate cumulative metrics for jar
-		
-		//urlCL.close();
-	}
-	
-	private static void getClasses(JarFile jf, Arraylist<String> classNames) throws IOException {
-		Enumeration<JarEntry> classes = jf.entries();
-		JarEntry elem;
-		while (classes.hasMoreElements()) {
-			elem = classes.nextElement();
-			if (elem.getName().endsWith(".class")) {
-				String className = elem.getName();
-				className.replace(".class", "");
-				className.replace("\\", ".");   // from windows to javapath
-				classNames.add(className);
-				System.out.println(className);
-			}
+		if (!file.exists()) {
+			System.out.println("File not found: " + fileName + "\n" + file.getAbsolutePath());
 		}
+		
+		JarFile jf = new JarFile(file);
+		System.out.println("Java archive: " + jf.getName()); // get the name of the jar file
+		
+		Collection<Class<?>> classes = new ArrayList<Class<?>>();
+		ArrayList<String> entryNames = new ArrayList<>();
+		Enumeration<JarEntry> jfEntries = jf.entries();
+		
+		while (jfEntries.hasMoreElements()) {
+			
+			JarEntry j = jfEntries.nextElement();
+			
+			// if there is a class, scan it
+			if (j.getName().endsWith(".class")) {
+				
+				String newClassName = j.getName().replace(".class","");
+				entryNames.add(newClassName);
+				newClassName = newClassName.replace("/", ".");
+				System.out.println(newClassName);
+				
+				// generate URL for accessing the each class
+				String url = "file://" + System.getProperty("user.dir") + "/" + j.getName();
+				
+				URL classURL = null;
+				try {
+					classURL = new URL(url);
+				} catch (MalformedURLException e) {
+					System.out.println("URL could not be parsed '" + url + "'");
+					e.printStackTrace();
+				}
+//				System.out.println("classURL: " + classURL.toString());
+				URLClassLoader cl = new URLClassLoader(new URL[] { classURL });
+				URL[] u = cl.getURLs();
+		
+				Class<?> c = null;
+				try {
+					c = cl.loadClass(newClassName);
+				} catch (ClassNotFoundException e) {
+					System.out.println("Could not find " + newClassName);
+					e.printStackTrace();
+				}
+
+				
+//				Class<?> c = Class.forName(newClassName);
+//				classes.add(newClassName);		
+						
+			} else if (j.getName().endsWith(".jar")) {
+				System.out.println("THERES ANOTHER JAR: " + j.getName());
+//				checkJar(j.getName()); // open the archive recursively
+			}		
+		}
+
 		jf.close();
+		
+
+//		for (int i = 0; i < entryNames.size(); i++) {
+//			System.out.println("    class: " + entryNames.get(i));
+//		}
+		
+		
 	}
 
 }
